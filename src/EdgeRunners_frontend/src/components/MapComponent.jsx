cd85f4b55db878762289fb2273cbbb5f070cba8e
LeislaCV <utm23090765@utma.edu.mx>
2024-10-30 14:45:01 -0600

    Py

--- src/EdgeRunners_frontend/src/components/MapComponent.jsx
+++ src/EdgeRunners_frontend/src/components/MapComponent.jsx
@@ -1,18 +1,15 @@
-// src/components/MapComponent.js
-
 import React from 'react';
 import { GoogleMap, LoadScript, Marker, Polyline } from '@react-google-maps/api';
 
-// Estilo del contenedor del mapa
+
 const containerStyle = {
   width: '100%',
   height: '400px'
 };
 
-// Centro del mapa
 const center = {
-  lat: 21.8781, // Cambia esto a la latitud inicial deseada
-  lng: -102.2915 // Cambia esto a la longitud inicial deseada
+  lat: 21.8781, 
+  lng: -102.2915 
 };
 
 const MapComponent = ({ incidents, patrolRoutes }) => {
@@ -23,25 +20,25 @@ const MapComponent = ({ incidents, patrolRoutes }) => {
         center={center}
         zoom={12}
       >
-        {/* Dibujar marcadores para los incidentes */}
+        {}
         {incidents.map((incident) => (
           <Marker 
             key={incident._id} 
             position={{ lat: incident.location.coordinates[1], lng: incident.location.coordinates[0] }} 
             icon={{
-              url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png", // Icono rojo para incidentes
+              url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png", 
               scaledSize: new window.google.maps.Size(30, 30),
             }}
           />
         ))}
 
-        {/* Dibujar las rutas de patrullas */}
+        {}
         {patrolRoutes.map((route, index) => (
           <Polyline
             key={index}
             path={route}
             options={{
-              strokeColor: '#00FF00', // Color verde para las rutas
+              strokeColor: '#00FF00', 
               strokeOpacity: 0.7,
               strokeWeight: 4,
               zIndex: 1,
